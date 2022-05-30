d26fc8c96db3335469b8b8d81d2f204ea008d0a6
Hansel <hchavezblanco@hotmail.com>
2022-05-30 00:31:38 -0600

    small stuff

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var submit = document.getElementById("submit");
 var maps = document.getElementById("maps");
 var viewmap = document.getElementById("viewmap");
 
-maps.value = "05_25_22";
+maps.value = "05_30_22";
 viewmap.setAttribute("src", `./${maps.value}/unmined.index.html`);
 
 submit.onclick = () => {
