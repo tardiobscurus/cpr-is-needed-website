ee3fc0a94b3b3861f1a8148859f1b9ba6789529e
Hansel <hchavezblanco@hotmail.com>
2022-05-30 00:29:12 -0600

    updated map

---
+++ 05_30_22/custom.markers.js
@@ -0,0 +1,71 @@
+UnminedCustomMarkers = {
+
+    isEnabled: true,
+
+    markers: [
+        // Towns, Islands, and/or other major places.
+        {
+            x: -67,
+            z: 448,
+            image: "../capital_star.png",
+            imageScale: .25,
+            offsetY: -45,
+            text: "Agarico\n(Spawn Island)",
+            textColor: "black",
+            font: "bold 25px Calibri, sans serif",
+        },
+        {
+            x: -2149,
+            z: -276,
+            image: "../capital_star.png",
+            imageScale: .25,
+            offsetY: -25,
+            text: "Bida",
+            textColor: "black",
+            font: "bold 25px Calibri, sans serif"
+        },
+        {
+            x: -617,
+            z: 711,
+            text: "Kope Isle",
+            image: "../small_town_star.png",
+            imageScale: .20,
+            offsetY: -20,
+            textColor: "black",
+            font: "bold 20px Calibri, sans serif"
+        },
+
+        // Villages
+
+        {
+            x: -1451,
+            z: -203,
+            text: "Bida Village",
+            textColor: "black",
+            font: "bold 15px Calibri, sans serif"
+        },
+
+        // Caverns
+        {
+            x: -2010,
+            z: -18,
+            text: "Chavez Caverns",
+            textColor: "yellow",
+            font: "bold 15px Calibri, sans serif"
+        },
+        {
+            x: -1314,
+            z: 566,
+            text: "Flores Caverns",
+            textColor: "yellow",
+            font: "bold 15px Calibri, sans serif"
+        },
+        {
+            x: -580,
+            z: 635,
+            text: "Cortiz Caverns",
+            textColor: "yellow",
+            font: "bold 15px Calibri, sans serif"
+        },
+    ]
+}
